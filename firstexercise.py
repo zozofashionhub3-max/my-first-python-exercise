cf128f92f38beab68e9f198bd6129b45a26ea84f
zozofashionhub3-max <zozofashionhub3@gmail.com>
2025-10-22 19:46:49 +0200

    Create firstexercise.py with introductory prints
    Add a simple Python program with introductory messages.

---
+++ firstexercise.py
@@ -0,0 +1,6 @@
+# My first Python program
+print("Hello, Team Girlcoders! 🌍")
+print("My name is Sinazo Maseti, and I just wrote my first Python program!")
+print("Python makes coding fun and easy to learn 😄")
+ my-first-python-exercise
+This is my first python exercise
